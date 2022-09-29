968ff1c575943ef6ca9fdd61e88f4eb3e418563e
Yasadu De Silva <49260501+lg4492@users.noreply.github.com>
2022-09-29 14:43:07 -0400

    Added honey/indication installation script
    Added honey/indication installation script for use in recycling process. Deleted Container Creation folder.

---
+++ Recycling/container_honey_and_indication_script.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+containerName=$1
+
+# indications: 1 for none, 2 for snoopylogger, 3 for message, 4 for both
+indicationOption=$2
+
+# copy honey files over from honey folder on host
+sudo cp -r ./honey/* /var/lib/lxc/$containerName/rootfs/root/
+
+# install ssh on container
+sudo lxc-attach -n $1 -- bash -c "sudo apt-get -qq update"
+sudo lxc-attach -n $1 -- bash -c "sudo apt-get -qq install openssh-server"
+
+# install snoopylogger if requested
+if [[ $indicationOption -eq 2 ]] || [[ $indicationOption -eq 4 ]]
+then
+  sudo lxc-attach -n $containerName -- bash -c "sudo apt-get install wget -y"
+  sudo lxc-attach -n $containerName -- bash -c "sudo wget -O install-snoopy.sh https://github.com/a2o/snoopy/raw/install/install/install-snoopy.sh"
+  sudo lxc-attach -n $containerName -- bash -c "chmod 755 install-snoopy.sh"
+  sudo lxc-attach -n $containerName -- bash -c "sudo ./install-snoopy.sh stable"
+  sudo lxc-attach -n $containerName -- bash -c "rm -rf ./install-snoopy.* snoopy-*"
+fi
+
+# install ssh warning banner and place banner file inside container if requested
+if [[ $indicationOption -eq 3 ]] || [[ $indicationOption -eq 4 ]]
+then
+  sudo lxc-attach -n $containerName -- bash -c "echo \"Banner /etc/warning_banner\" >> /etc/ssh/sshd_config"
+  sudo cp ./ssh_banner_info/warning_banner /var/lib/lxc/$containerName/rootfs/etc/
+  sudo lxc-attach -n $containerName -- bash -c "sudo systemctl restart ssh"
+fi
