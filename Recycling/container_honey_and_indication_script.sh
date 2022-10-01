727d793c82faa9ee517cc4145dd691717bc7755c
Kaiwen Song <31936108+Kaidriver@users.noreply.github.com>
2022-10-01 00:12:19 -0400

    combine recycling scripts

--- Recycling/container_honey_and_indication_script.sh
+++ Recycling/container_honey_and_indication_script.sh
@@ -2,9 +2,6 @@
 
 containerName=$1
 
-# indications: 1 for none, 2 for snoopylogger, 3 for message, 4 for both
-indicationOption=$2
-
 # copy honey files over from honey folder on host
 sudo cp -r ./honey/* /var/lib/lxc/$containerName/rootfs/root/
 
@@ -13,7 +10,7 @@ sudo lxc-attach -n $1 -- bash -c "sudo apt-get -qq update"
 sudo lxc-attach -n $1 -- bash -c "sudo apt-get -qq install openssh-server"
 
 # install snoopylogger if requested
-if [[ $indicationOption -eq 2 ]] || [[ $indicationOption -eq 4 ]]
+if [[ $containerName == "honeypot2" ]] || [[ $containerName == "honeypot4" ]]
 then
   sudo lxc-attach -n $containerName -- bash -c "sudo apt-get install wget -y"
   sudo lxc-attach -n $containerName -- bash -c "sudo wget -O install-snoopy.sh https://github.com/a2o/snoopy/raw/install/install/install-snoopy.sh"
@@ -23,9 +20,10 @@ then
 fi
 
 # install ssh warning banner and place banner file inside container if requested
-if [[ $indicationOption -eq 3 ]] || [[ $indicationOption -eq 4 ]]
+if [[ $containerName == "honeypot3" ]] || [[ $containerName == "honeypot4" ]]
 then
   sudo lxc-attach -n $containerName -- bash -c "echo \"Banner /etc/warning_banner\" >> /etc/ssh/sshd_config"
   sudo cp ./ssh_banner_info/warning_banner /var/lib/lxc/$containerName/rootfs/etc/
   sudo lxc-attach -n $containerName -- bash -c "sudo systemctl restart ssh"
 fi
+
--- Recycling/recycler.sh
+++ Recycling/recycler.sh
@@ -12,7 +12,6 @@ if [ $status -ne 0 ]
 then
   IP=$(sudo lxc-info -n $1 -iH)
   sudo forever stop $1
-  sudo forever stop $1
   sudo iptables --table nat --delete PREROUTING --source 0.0.0.0/0 --destination $2 --jump DNAT --to-destination $IP
   sudo iptables --table nat --delete POSTROUTING --source $IP --destination 0.0.0.0/0 --jump SNAT --to-source $2
   sudo iptables --table nat --delete PREROUTING --source 0.0.0.0/0 --destination $2 --protocol tcp --dport 22 --jump DNAT --to-destination 127.0.0.1:$port
@@ -23,12 +22,12 @@ fi
 
 sudo lxc-create -n $1 -t download -- -d ubuntu -r focal -a amd64
 sudo lxc-start -n $1
-sudo lxc-attach -n $1 -- bash -c "apt install openssh-server"
+
 sleep 5s
+./container_honey_and_indication_script.sh $1
 IP=$(sudo lxc-info -n $1 -iH)
 sudo forever -l /home/student/data/$1_log --id $1 -a start /home/student/MITM/mitm.js -n $1 -i $IP -p $port --auto-access --auto-access-fixed 3 --debug & echo "pid $!"
 
-echo $IP
 sudo ip link set dev enp4s2 up
 sudo ip addr add $2/16 brd + dev enp4s2
 sudo iptables --table nat --insert PREROUTING --source 0.0.0.0/0 --destination $2 --jump DNAT --to-destination $IP
