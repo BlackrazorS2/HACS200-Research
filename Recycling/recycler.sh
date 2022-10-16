8313b43658197b1f62a826477ecc5319e516eeb5
Random_Dracolich <45239440+BlackrazorS2@users.noreply.github.com>
2022-10-16 17:06:16 -0400

    Integrating data collection scripts

--- Recycling/recycler.sh
+++ Recycling/recycler.sh
@@ -10,6 +10,8 @@ status=$(sudo lxc-ls | grep $1 | wc -l)
 
 if [ $status -ne 0 ]
 then
+  sudo python3 /home/student/data/gatherer.py $1
+  sudo python3 /home/student/data/fileSieve.py
   IP=$(sudo lxc-info -n $1 -iH)
   sudo forever stop $1
   sudo echo "IP:  $IP"
