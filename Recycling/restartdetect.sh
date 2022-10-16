5ff6dad103211e43646594323e89067b67ec121f
nosaj9 <90656001+nosaj9@users.noreply.github.com>
2022-10-16 17:30:28 -0400

    Update restartdetect.sh

--- Recycling/restartdetect.sh
+++ Recycling/restartdetect.sh
@@ -6,22 +6,22 @@ killall inotifywait
 
 echo "Killed all exit listeners"
 
-if [ -f DATABASE_1_log ]
+if [ -f data/DATABASE_1_log ]
 then
         ./detectexit.sh 1 &
 fi
 
-if [ -f DATABASE_2_log ]
+if [ -f data/DATABASE_2_log ]
 then
         ./detectexit.sh 2 &
 fi
 
-if [ -f DATABASE_3_log ]
+if [ -f data/DATABASE_3_log ]
 then
         ./detectexit.sh 3 &
 fi
 
-if [ -f DATABASE_4_log ]
+if [ -f data/DATABASE_4_log ]
 then
         ./detectexit.sh 4 &
 fi
