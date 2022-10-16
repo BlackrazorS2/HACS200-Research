a1c3d4306a59a03f5c2732b192115f8d98dbdc76
Yasadu De Silva <49260501+lg4492@users.noreply.github.com>
2022-10-16 18:15:50 -0400

    Update detectexit.sh

--- Recycling/detectexit.sh
+++ Recycling/detectexit.sh
@@ -5,8 +5,9 @@ inotifywait -q -m -e close_write data/DATABASE_$1_log |
 while read -r filename event; do
         if tail -1 "$filename" | grep -q "Attacker closed connection";
         then
+                honeypot_ip=$(sudo cat ./properties/DATABASE_$1_properties | cut -d' ' -f1)
                 # copy the MITM logs to our data collection directory.
                 # copy modified files to a private container for potential future analysis.
-                ./recycler.sh
+                ./recycler.sh DATABASE_$1 $honeypot_ip 1
         fi
 done
