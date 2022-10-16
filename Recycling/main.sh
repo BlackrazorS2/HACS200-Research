8a16535f01c1d2f46c6f9e0e06273179818166d0
Yasadu De Silva <49260501+lg4492@users.noreply.github.com>
2022-10-16 19:13:14 -0400

    fixed syntax errors

--- Recycling/main.sh
+++ Recycling/main.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 sudo ./container_rotate.sh 0 0 1
-sudo ./restartdetect
+sudo ./restartdetect.sh
--- Recycling/restartdetect.sh
+++ Recycling/restartdetect.sh
@@ -9,25 +9,25 @@ sudo echo "Killed all exit listeners"
 if [ -f data/DATABASE_1_log ]
 then
         sudo echo "detectexit 1"
-        sudo bash ./detectexit.sh 1 &
+        sudo ./detectexit.sh 1 &
 fi
 
 if [ -f data/DATABASE_2_log ]
 then
         sudo echo "detectexit 2"
-        sudo bash detectexit.sh 2 &
+        sudo ./detectexit.sh 2 &
 fi
 
 if [ -f data/DATABASE_3_log ]
 then
         sudo echo "detectexit 3"
-        sudo bash detectexit.sh 3 &
+        sudo ./detectexit.sh 3 &
 fi
 
 if [ -f data/DATABASE_4_log ]
 then
         sudo echo "detectexit 4"
-        sudo bash detectexit.sh 4 &
+        sudo ./detectexit.sh 4 &
 fi
 
 sudo echo "Rebooted all exit listeners"
