601a1a0bebe9f97ae8c56d3768c3902c0e569d0d
nosaj9 <90656001+nosaj9@users.noreply.github.com>
2022-10-16 17:49:50 -0400

    Update restartdetect.sh

--- Recycling/restartdetect.sh
+++ Recycling/restartdetect.sh
@@ -2,28 +2,28 @@
 
 # kills all detectexit listeners for each honeypot and restarts them
 
-killall inotifywait
+sudo killall inotifywait
 
-echo "Killed all exit listeners"
+sudo echo "Killed all exit listeners"
 
 if [ -f data/DATABASE_1_log ]
 then
-        ./detectexit.sh 1 &
+        sudo bash detectexit.sh 1 &
 fi
 
 if [ -f data/DATABASE_2_log ]
 then
-        ./detectexit.sh 2 &
+        sudo bash detectexit.sh 2 &
 fi
 
 if [ -f data/DATABASE_3_log ]
 then
-        ./detectexit.sh 3 &
+        sudo bash detectexit.sh 3 &
 fi
 
 if [ -f data/DATABASE_4_log ]
 then
-        ./detectexit.sh 4 &
+        sudo bash detectexit.sh 4 &
 fi
 
-echo "Rebooted all exit listeners"
+sudo echo "Rebooted all exit listeners"
