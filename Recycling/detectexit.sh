cbe3924d12031a55c7140abc5e9cbb7c9a56b709
nosaj9 <90656001+nosaj9@users.noreply.github.com>
2022-10-17 01:32:40 -0400

    Update detectexit.sh

--- Recycling/detectexit.sh
+++ Recycling/detectexit.sh
@@ -3,7 +3,7 @@
 
 sudo inotifywait -q -m -e modify data/DATABASE_$1_log |
 while read -r filename event; do
-        if tail -1 "$filename" | grep -q "Attacker closed connection";
+        if tail -5 "$filename" | grep -q "Attacker closed connection";
         then
                 echo "honeypot $1 attacker exit"
                 honeypot_ip=$(sudo cat ./properties/DATABASE_$1_properties | cut -d' ' -f1)
