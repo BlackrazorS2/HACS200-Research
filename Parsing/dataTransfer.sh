c7daa0c1f6a013596cf27314ed140fd2d9a4650c
Random_Dracolich <45239440+BlackrazorS2@users.noreply.github.com>
2022-10-25 15:32:38 -0400

    Update dataTransfer.sh

--- Parsing/dataTransfer.sh
+++ Parsing/dataTransfer.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # takes three arguments being the username, paswword and ip of the host
-if [$# -ne 3]
+if [ $# -ne 3 ]
 then
     echo "Usage: dataTransfer.sh [username] [password] [ip]"
     exit 1
@@ -9,9 +9,4 @@ fi
 
 path="/home/student/data/copied_data" # extra workstation vm
 
-
-sudo rsync -avuh -e ssh "$path/*" $1@$3:/home/$1/HACS200-Research-Data-Backup/
-expect "password:"
-send $2
-expect "*\r"   
-expect "\r"  
+sudo sshpass -p $2 scp -r $1 "$path/*" $1@$3:/home/$1/HACS200-Research-Data-Backup/
